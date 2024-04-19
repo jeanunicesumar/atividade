0c74abfed0a6e5c9752b9f1f1c9879335c6cda51
jeanunicesumar <124851594+jeanunicesumar@users.noreply.github.com>
2024-04-19 01:17:48 +0000

    Finalizando

--- pages/todo/index.js
+++ pages/todo/index.js
@@ -1,11 +1,42 @@
+document.addEventListener('DOMContentLoaded', function () {
+    main()
+})
 
-const button = document.querySelector('#button');
-const field = document.querySelector('#field');
+function main() {
 
-button.addEventListener("click", saveField());
+    const button = document.getElementById('button');
+    const field = document.getElementById('field');
 
-function saveField() {
+    button.addEventListener("click", (e) =>  {
+        saveField(field);
+    });
 
-    console.log(field)
+    showTasks();
+}
+
+function saveField(field) {
+
+    if (!field.value) {
+        return;
+    }
+
+    const fields = JSON.parse(localStorage.getItem('fields')) || [];
+    fields.push(field.value);
+    localStorage.setItem('fields', JSON.stringify(fields));
+
+    showTasks();
+}
+
+function showTasks() {
+
+    const tasks = document.querySelector('#tasks');
+    tasks.innerHTML = '';
+    const fields = JSON.parse(localStorage.getItem('fields')) || [];
+
+    fields.forEach(field => {
+        const p = document.createElement("p");
+        p.textContent = field;
+        tasks.appendChild(p);
+    });
 
 }
