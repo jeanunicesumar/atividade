cb3f38d8f890ae282780d97ccad218878032d202
jeanunicesumar <124851594+jeanunicesumar@users.noreply.github.com>
2024-04-19 00:40:05 +0000

    Teste

---
+++ pages/todo/index.js
@@ -0,0 +1,11 @@
+
+const button = document.querySelector('#button');
+const field = document.querySelector('#field');
+
+button.addEventListener("click", saveField());
+
+function saveField() {
+
+    console.log(field)
+
+}
